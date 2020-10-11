18c355e9bd67ff462e092b4b3ccfe948d371190d
ktysakura <563073320@qq.com>
2020-10-11 22:03:53 +0800

    sdl代码修改

--- main.cpp
+++ main.cpp
@@ -24,7 +24,7 @@ extern int testAacEncoder(const char *pcmFileName, const char* aacFileName);
 //#define RTMP_URL "rtmp://111.229.231.225/live/livestream"
 #define RTMP_URL "rtmp://192.168.1.170/live/livestream"
 // ffmpeg -re -i  rtmp_test_hd.flv  -vcodec copy -acodec copy  -f flv -y rtmp://111.229.231.225/live/livestream
-//// ffmpeg -re -i  1920x832_25fps.flv  -vcodec copy -acodec copy  -f flv -y rtmp://111.229.231.225/live/livestream
+//// ffmpeg -re -i  1920x832_25fps.flv  -vcodec copy -acodec copy  -f flv -y rtmp://192.168.1.170/live/livestream
 int main(int argc, char* argv[])
 {
 
--- videooutsdl.cpp
+++ videooutsdl.cpp
@@ -1,6 +1,5 @@
 ﻿#include "videooutsdl.h"
 namespace LQF {
-static bool bSDL_INIT_VIDEO = false;
 VideoOutSDL::VideoOutSDL()
 {
 
@@ -8,104 +7,77 @@ VideoOutSDL::VideoOutSDL()
 
 VideoOutSDL::~VideoOutSDL()
 {
-    if(texture_)
-        SDL_DestroyTexture(texture_);
-    if(renderer_)
-        SDL_DestroyRenderer(renderer_);
-    if(win_)
-        SDL_DestroyWindow(win_);
+    if(texture)
+        SDL_DestroyTexture(texture);
+    if(renderer)
+        SDL_DestroyRenderer(renderer);
+    if(win)
+        SDL_DestroyWindow(win);
 
     SDL_Quit();
 }
 
 RET_CODE VideoOutSDL::Init(const Properties &properties)
 {
-    video_width_ = properties.GetProperty("video_width", -1);
-    if(-1 == video_width_)
-    {
-        LogError("video_width no set");
-        return RET_FAIL;
-    }
-    video_height_ = properties.GetProperty("video_height", -1);
-    if(-1 == video_height_)
-    {
-        LogError("video_height no set");
-        return RET_FAIL;
-    }
 
-    pixformat_ = properties.GetProperty("pixformat",SDL_PIXELFORMAT_IYUV);
-    win_x_ = properties.GetProperty("win_x", SDL_WINDOWPOS_UNDEFINED);
-    win_y_ = properties.GetProperty("win_y", SDL_WINDOWPOS_UNDEFINED);
-    win_width_ = properties.GetProperty("win_width", video_width_);
-    win_height_ = properties.GetProperty("win_height", video_height_);
-    win_title_  = properties.GetProperty("win_title", "sdl display");
     //初始化 SDL
     if(SDL_Init(SDL_INIT_VIDEO))
     {
         LogError("Could not initialize SDL - %s", SDL_GetError());
         return RET_FAIL;
     }
+
+    int x = properties.GetProperty("x", SDL_WINDOWPOS_UNDEFINED);
     //创建窗口
-    win_ = SDL_CreateWindow(win_title_.c_str(),
-                           win_x_,
-                           win_y_,
-                           win_width_, win_height_,
-                           SDL_WINDOW_RESIZABLE);
-
-
-    if(!win_)
+    win = SDL_CreateWindow("Simplest YUV Player",
+                           x,
+                           SDL_WINDOWPOS_UNDEFINED,
+                           video_width, video_height,
+                           SDL_WINDOW_OPENGL|SDL_WINDOW_RESIZABLE);
+    if(!win)
     {
         LogError("SDL: could not create window, err:%s",SDL_GetError());
         return RET_FAIL;
     }
 
     // 基于窗口创建渲染器
-    renderer_ = SDL_CreateRenderer(win_, -1, 0);
-    if(!renderer_)
+    renderer = SDL_CreateRenderer(win, -1, 0);
+    if(!renderer)
     {
-        LogError("SDL: could not create renderer_, err:%s",SDL_GetError());
+        LogError("SDL: could not create renderer, err:%s",SDL_GetError());
         return RET_FAIL;
     }
     // 基于渲染器创建纹理
-    texture_ = SDL_CreateTexture(renderer_,
-                                pixformat_,
+    texture = SDL_CreateTexture(renderer,
+                                pixformat,
                                 SDL_TEXTUREACCESS_STREAMING,
-                                video_width_,
-                                video_height_);
-    if(!texture_)
+                                video_width,
+                                video_height);
+    if(!texture)
     {
-        LogError("SDL: could not create texture_, err:%s",SDL_GetError());
+        LogError("SDL: could not create texture, err:%s",SDL_GetError());
         return RET_FAIL;
     }
-    LogInfo("%s init done", win_title_.c_str());
     return RET_OK;
 }
 
-RET_CODE VideoOutSDL::Output(const uint8_t *video_buf, const uint32_t size)
+RET_CODE VideoOutSDL::Output(uint8_t *video_buf, uint32_t size)
 {
     // 设置纹理的数据
-    SDL_UpdateTexture(texture_, NULL, video_buf, video_width_);
+    SDL_UpdateTexture(texture, NULL, video_buf, video_width);
 
     //FIX: If window is resize
-    rect_.x = 0;
-    rect_.y = 0;
-    rect_.w = video_width_;
-    rect_.h = video_height_;
+    rect.x = 0;
+    rect.y = 0;
+    rect.w = video_width;
+    rect.h = video_height;
 
     // 清除当前显示
-    if(SDL_RenderClear(renderer_) != 0)
-    {
-        LogError("SDL_RenderClear failed");
-        return RET_FAIL;
-    }
+    SDL_RenderClear(renderer);
     // 将纹理的数据拷贝给渲染器
-    if(SDL_RenderCopy(renderer_, texture_, NULL, &rect_) != 0)
-    {
-        LogError("SDL_RenderCopy failed");
-        return RET_FAIL;
-    }
+    SDL_RenderCopy(renderer, texture, NULL, &rect);
     // 显示
-    SDL_RenderPresent(renderer_);
+    SDL_RenderPresent(renderer);
     return RET_OK;
 }
 
